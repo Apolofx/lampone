b91e142485b76eb960033aa654729139931175dc
Juan Ignacio Consolani <nacho.conso@gmail.com>
2021-06-20 00:56:38 -0300

    fix prepare script

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
-import {SetPrettier} from './lib/scripts'
-SetPrettier()
+import { SetPrettier } from "./lib/scripts";
+//TODO use yarg or any other library to parse cli args
+const precommit = Boolean(process.argv[2]);
 
+SetPrettier(precommit);
 
 //TODO add cli to set script options
--- lib/scripts.ts
+++ lib/scripts.ts
@@ -1,9 +1,38 @@
-import { exec, execSync } from "child_process";
+import { execSync } from "child_process";
+import fs from "fs";
 
-function SetPrettier(){
-    execSync('npm install -D prettier',{stdio:[0,1,2]})   
-    // exec('npm install -D prettier')   
-    // exec('npm install -D prettier')   
+//---PRETTIER---//
+function SetPrettier(precommit = false) {
+  const rawPackageJSON = fs.readFileSync(`package.json`);
+  const packageJSON = JSON.parse(rawPackageJSON.toString());
+  //install
+  execSync("npm install -D prettier", { stdio: [0, 1, 2] });
+
+  //create prettier config and prettier ignore
+  const ignored = "node_modules\nbuild\ndist\ncoverage\nassets";
+  fs.writeFileSync(".prettierrc.json", JSON.stringify({}));
+  fs.writeFileSync(".prettierignore", ignored);
+
+  //format already written code
+  execSync("npx prettier --write .", { stdio: [0, 1, 2] });
+
+  //setup precommit
+  if (precommit) {
+    //TODO check for husky and lint-staged already in package.json
+    execSync("npm install --save-dev husky lint-staged", { stdio: [0, 1, 2] });
+    execSync("npx husky install", { stdio: [0, 1, 2] });
+    if (!packageJSON["scripts"]["prepared"]) {
+      packageJSON["scripts"]["prepared"] = "husky install";
+    }
+    execSync("npx husky add .husky/pre-commit 'npx lint-staged'", {
+      stdio: [0, 1, 2],
+    });
+
+    packageJSON["lint-staged"] = {
+      "**/*": "prettier --write --ignore-unknown",
+    };
+    fs.writeFileSync("package.json", JSON.stringify(packageJSON));
+  }
 }
 
-export {SetPrettier}
+export { SetPrettier };
