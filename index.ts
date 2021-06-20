d3e748b2116bb662b855bf72adf04adbbd0cfc99
Juan Ignacio Consolani <nacho.conso@gmail.com>
2021-06-20 12:09:16 -0300

    change name

--- index.ts
+++ index.ts
@@ -1,19 +1,18 @@
 #!/usr/bin/env node
 import figlet from "figlet";
-import filget from "figlet";
 import { SetPrettier } from "./lib/scripts";
-const precommit = Boolean(process.argv[2]);
 
 console.log(
-  figlet.textSync("Apolo", {
+  figlet.textSync("Jaime", {
     font: "Larry 3D",
     horizontalLayout: "default",
     verticalLayout: "default",
     width: 80,
     whitespaceBreak: true,
-  })
+  }),
 );
 
+const precommit = Boolean(process.argv[2]);
 SetPrettier(precommit);
 
 //TODO use yarg or any other library to parse cli args
